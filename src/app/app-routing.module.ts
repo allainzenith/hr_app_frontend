5efc39603d2d56f81954b9be71f42280be3a5d0b
allainzenith <allainzenith12@gmail.com>
2023-04-25 09:35:58 +0800

    get request for ticket

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginSignupComponent } from './pages/login-signup/login-signup.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
+import { ReportsComponent } from './pages/reports/reports.component';
+import { HeaderComponent } from './components/header/header.component';
 const routes: Routes = [
   { path: '', component: LoginSignupComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'header', component: HeaderComponent },
 ];
 
 @NgModule({
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  constructor(private http: HttpClient){
+  
+  }
+  async ngOnInit() {
+    this.getAllTickets();
+  }
+  
+  async token(){
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Use the token value here
+      console.log('token: '+token);
+    } else {
+      // Handle the case where the token value is not present in localStorage
+    }
+
+    return token;
+  }
+
+
+  async getAllTickets(){
+    const token = this.token();
+
+    const options = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    
+    this.http.get('http://localhost:8080/spring-hibernate-jpa/ticket/empID/1232', options).subscribe(response => {
+      // Handle the response here
+      console.log(response);
+      // console.log(response.created_at);
+    }, error => {
+      // Handle errors here
+    });
+
+  }
 }
