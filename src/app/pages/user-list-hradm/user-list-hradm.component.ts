3e6a1829382eb05f3f249bbc8edd53104a772238
chstersaagundo <chesteracesaagundo@gmail.com>
2023-04-28 11:38:42 +0800

    updates

--- src/app/pages/user-list-hradm/user-list-hradm.component.ts
+++ src/app/pages/user-list-hradm/user-list-hradm.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { ModalserviceService } from 'src/app/service/modalservice.service';
 @Component({
   selector: 'app-user-list-hradm',
@@ -6,8 +8,70 @@ import { ModalserviceService } from 'src/app/service/modalservice.service';
   styleUrls: ['./user-list-hradm.component.css']
 })
 export class UserListHradmComponent {
-  constructor(public modalService: ModalserviceService){}
-  ngOnInit(){
+  response:any;
+
+
+  constructor(private http: HttpClient, private router: Router, public modalService: ModalserviceService){}
+  async ngOnInit() {
+    
+    this.getAllEmployee();
+  }
+  
+  async token(){
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Use the token value here
+      console.log('token: '+token);
+    } else {
+      // Handle the case where the token value is not present in localStorage
+    }
+
+    return token;
+  }
+
+  async empID(){
+    const empID = localStorage.getItem('empID');
+    if (empID) {
+      console.log('empID: '+empID);
+    } else {
+      // Handle the case where the token value is not present in localStorage
+    }
+
+    return empID;
+  }
+
+  async emp_role(){
+    const emp_role = localStorage.getItem('emp_role');
+    if (emp_role) {
+      console.log('empID: '+emp_role);
+    } else {
+      // Handle the case where the token value is not present in localStorage
+    }
+
+    return emp_role;
+  }
+
+
+  async getAllEmployee(){
+    const token = this.token();
+
+    const options = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    
+    this.http.get(`http://localhost:8080/spring-hibernate-jpa/employee/getall`, options).subscribe(response => {
+      // Handle the response here
+      console.log(response)
+      this.response = Object.values(response);
+      console.log(this.response)
+      // console.log(response.data[0].ticketID)      
+      // console.log(response.created_at);
+    }, error => {
+      // Handle errors here
+    });
 
   }
+  
 }
